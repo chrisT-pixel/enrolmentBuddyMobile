59d5ca3296a0a25bf0ad30a32b843dbdb35259ab
chrisT-pixel <53413756+chrisT-pixel@users.noreply.github.com>
2020-09-01 21:28:41 +0930

    mainscreen simple interface

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={{ width: 305, height: 90 }} />
+      <Text style={{ fontSize: 22, color: 'black', margin: 5, backgroundColor: '#efefef', padding: 8, width: 305, }}>Enrolment Buddy</Text>
+
+      <Text style={{ fontSize: 26, color: 'black', margin: 15 }}>Options for Students</Text>
+
+      <TouchableOpacity
+       onPress={() => alert('Option 1 selected!')}
+       style={{ backgroundColor: 'black', marginBottom: 8,}}>
+       <Text style={{ fontSize: 20, color: '#fff', padding: 8, width: 305, textAlign: 'center'}}>Option 1</Text>
+       </TouchableOpacity>
+
+       <TouchableOpacity
+        onPress={() => alert('Option 2 selected!')}
+        style={{ backgroundColor: 'black', marginBottom: 8, }}>
+        <Text style={{ fontSize: 20, color: '#fff', padding: 8, width: 305, textAlign: 'center' }}>Option 2</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+         onPress={() => alert('Option 3 selected!')}
+         style={{ backgroundColor: 'black', marginBottom: 8, }}>
+         <Text style={{ fontSize: 20, color: '#fff', padding: 8, width: 305, textAlign: 'center' }}>Option 3</Text>
+         </TouchableOpacity>
+
+      <StatusBar style="auto" />
+
+      <Text style={{ fontSize: 26, color: 'black', margin: 12 }}>Options for Lecturers</Text>
+      <TouchableOpacity
+       onPress={() => alert('Option 1 selected!')}
+       style={{ backgroundColor: '#be1428', marginBottom: 8,}}>
+       <Text style={{ fontSize: 20, color: '#fff', padding: 8, width: 305, textAlign: 'center'}}>Option 1</Text>
+       </TouchableOpacity>
+
+       <TouchableOpacity
+        onPress={() => alert('Option 2 selected!')}
+        style={{ backgroundColor: '#be1428', marginBottom: 8, }}>
+        <Text style={{ fontSize: 20, color: '#fff', padding: 8, width: 305, textAlign: 'center' }}>Option 2</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+         onPress={() => alert('Option 3 selected!')}
+         style={{ backgroundColor: '#be1428', marginBottom: 8, }}>
+         <Text style={{ fontSize: 20, color: '#fff', padding: 8, width: 305, textAlign: 'center' }}>Option 3</Text>
+         </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +60,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
   },
 });
